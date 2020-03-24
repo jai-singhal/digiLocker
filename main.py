6c8377e366e25ccede8ec828f44516c44a223eda
Jai Singhal <jaisinghal48@gmail.com>
2020-03-25 04:00:07 +0530

    add Login system

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
-from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+from flask import Flask, escape, request, session, redirect, url_for
+from flask import render_template
 
 
-@app.get("/")
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+import random, os, string
+# import sha3
+# import ethereum
 
+
+app = Flask(__name__)
+app.secret_key = b'OCML3BRawWEUeaxcuKHLpw'
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+# templates = Jinja2Templates(directory="templates")
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/api/login/metamask", methods = ['GET'])
+def login_api():
+    token = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for i in range(32))
+    session['login_token'] = token
+    return {"data": token, }
+    # eth_addr = recover_to_addr(token, signature)
+
+@app.route("/api/login/metamask", methods = ['POST'])
+def login_postapi():
+    token = session.get('login_token')
+    if not token:
+        return {'error': "No login token in session, please request token again by sending GET request to this url",
+                'success': False}
+    else:
+        session.pop("login_token", None)
+        signature = request.form.get("signature")
+        address = request.form.get("address")
+        print(signature, address)
+        return {'success': True, 'redirect_url': "/"}
+
+
+@app.route("/api/logout/metamask", methods = ['POST'])
+def logout():
+    session.pop("login_token", None)
+    return {'success': True, 'redirect_url': "/"}
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
+    # app.run(host="172.18.16.108", debug=True)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,92 +1,195 @@
-var transactionApproval = true;
-function validate(web3) {
-  if (typeof web3 !== 'undefined'){
-    console.log('MetaMask is installed')
-    web3.eth.getAccounts(function(err, accounts){
-      if (err != null) {
-          console.log(accounts)
-        console.log(err, accounts)
-      }
-      else if (accounts.length === 0) {
-        console.log('MetaMask is locked')
-      }
-      else {
-        console.log('MetaMask is unlocked')
-      
-        tokenInst.balanceOf(
-          web3.eth.accounts[0], 
-          function (error, result) {
-     
-          if (!error && result) {
-            var balance = result.c[0];
-            if (balance < dappCost * (100000000)) {
-              console.log('MetaMask has insufficient balance')
-              return false;
+var web3 = new Web3(window.web3.currentProvider);
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
             }
-            console.log('MetaMask has balance')
-            if (transactionApproval == true ){
-              requestApproval();
-              transactionApproval = false;
+        }
+    }
+    return cookieValue;
+}
+
+function loginWithSignature(address, signature, login_url, onLoginRequestError, onLoginFail, onLoginSuccess) {
+    console.log(address, signature, login_url)
+    var request = new XMLHttpRequest();
+    request.open('POST', login_url, true);
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            // Success!
+            var resp = JSON.parse(request.responseText);
+            if (resp.success) {
+                if (typeof onLoginSuccess == 'function') {
+                    onLoginSuccess(resp);
+                    console.log("Logined")
+                    $("#auth-btn a").html("Logout");
+                    $("#auth-btn").attr("btn-for", "logout");
+                    $("#auth-btn a").attr("href", "/api/logout/metamask");
+ 
+
+                }
+            } else {
+                if (typeof onLoginFail == 'function') {
+                    onLoginFail(resp);
+                }
             }
-          }
-          else {
-            console.error(error);
-          }
-          return false;
-        });
-      }
+        } else {
+            // We reached our target server, but it returned an error
+            alert("Login failed - request status " + request.status);
+            if (typeof onLoginRequestError == 'function') {
+                onLoginRequestError(request);
+            }
+        }
+    };
+
+    request.onerror = function () {
+        console.log("login failed - there was an error");
+        if (typeof onLoginRequestError == 'function') {
+            onLoginRequestError(request);
+        }
+        // There was a connection error of some sort
+    };
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+    var formData = 'address=' + address + '&signature=' + signature;
+    request.send(formData);
+}
+
+function checkWeb3(callback) {
+    web3.eth.getAccounts(function (err, accounts) { // Check for wallet being locked
+        if (err) {
+            throw err;
+        }
+        callback(accounts.length !== 0);
     });
-  } 
-  else{
-    console.log('MetaMask is not installed')
-  }
-}
-// request approval from MetaMask user
-function requestApproval() {
-  tokenInst.approve(
-    addrHOLD,
-    truePlanCost,
-    { gasPrice: web3.toWei('50', 'gwei') },
-    function (error, result) {
-      
-    if (!error && result) {
-      var data;
-      console.log('approval sent to network.');
-      var url = 'https://etherscan.io/tx/' + result;
-      var link = '<a href=\"" + url + "\" target=\"_blank\">View Transaction</a>';
-      console.log('waiting for approval ...');
-      data = {
-        txhash: result,
-        account_type: selectedPlanId,
-        txtype: 1, // Approval
-      };
-      apiService(data, '/transaction/create/', 'POST')
-      .done(function (response) {
-        location.href = response.tx_url;
-      });
-    }
-    else {
-      console.error(error);
-      console.log('You rejected the transaction');
-    }
-  });
 }
 
+function web3Login(login_url, onTokenRequestFail, onTokenSignFail, onTokenSignSuccess, // used in this function
+    onLoginRequestError, onLoginFail, onLoginSuccess) {
+    // used in loginWithSignature
+
+    // 1. Retrieve arbitrary login token from server
+    // 2. Sign it using web3
+    // 3. Send signed message & your eth address to server
+    // 4. If server validates that you signature is valid
+    // 4.1 The user with an according eth address is found - you are logged in
+    // 4.2 The user with an according eth address is NOT found - you are redirected to signup page
+
+    if (typeof web3 === 'undefined') {
+        alert('MetaMask is not installed');
+        return false;
+    }
+
+    var request = new XMLHttpRequest();
+    request.open('GET', login_url, true);
+
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            // Success!
+            var resp = JSON.parse(request.responseText);
+            var token = resp.data;
+            var msg = web3.utils.toHex(token);
+
+            web3.eth.getAccounts(function (err, accounts) {
+                if (err != null) {
+                    console.log(err);
+                    return false;
+                } else if (accounts.length === 0) {
+                    alert('MetaMask is locked');
+                    return false;
+                } else {
+                    var from = accounts[0]
+                    console.log("token: ", msg);
+                    console.log("from: ", from)
+
+                    $("#account-address").html("Your account address is: " + from);
+                    web3.eth.getBalance(from, (err, balance) => {
+                        balance = web3.utils.fromWei(balance, "ether")
+                        $("#funds").html("Your current balance is: " + balance + " ETH");
+                        if (balance == 0) {
+                            alert("Insufficient funds in your account. Total balance = 0 ETH");
+                            return false;
+                        } else {
+                            web3.eth.personal.sign(msg, from, function (err, result) {
+                                if (err) {
+                                    if (typeof onTokenSignFail == 'function') {
+                                        alert(err.message);
+                                        onTokenSignFail(err);
+                                    }
+                                    alert("Failed signing message \n" + msg + "\n - " + err);
+                                } else {
+                                    console.log("Signed message: " + result);
+                                    if (typeof onTokenSignSuccess == 'function') {
+                                        onTokenSignSuccess(result);
+                                    }
+                                    loginWithSignature(from, result, login_url, onLoginRequestError, onLoginFail, onLoginSuccess);
+                                }
+                            });
+
+                        }
+                    });
+
+                }
+            });
 
 
-$(document).ready(function(){
-    // console.log(web3)
-    let web3 = new Web3();
-    var account = web3.currentProvider
-    console.log(account, web3)
-    validate(web3);
-    // var accountInterval = setInterval(function() {
-    //   if (web3.eth.accounts[0] !== account) {
-    //     account = web3.eth.accounts[0];
-    // console.log(account, web3)
-        
-    //     // updateInterface();
-    //   }
-    // }, 100);
+        } else {
+            // We reached our target server, but it returned an error
+            alert("Login failed - request status " + request.status);
+            if (typeof onTokenRequestFail == 'function') {
+                onTokenRequestFail(request);
+                alert(request.status)
+            }
+        }
+    };
+
+    request.onerror = function () {
+        // There was a connection error of some sort
+        alert("Login failed - there was an error");
+        if (typeof onTokenRequestFail == 'function') {
+            onTokenRequestFail(request);
+        }
+    };
+    request.send();
+}
+
+
+
+$("#auth-btn").click(function (e) {
+    e.preventDefault();
+
+    if($(this).attr("btn-for") === "logout"){
+        console.log("Logout");
+        $("#account-address").html("");
+        $("#funds").html("");
+        $("#auth-btn a").html("Login");
+        $("#auth-btn").attr("btn-for", "login");
+        $("#auth-btn a").attr("href", "/api/login/metamask");
+        return false;
+    }
+
+    if (typeof web3 !== 'undefined') {
+        checkWeb3(function (loggedIn) {
+            if (!loggedIn) {
+                alert("Please unlock/login to your web3 provider (probably, Metamask)")
+            } else {
+                var login_url = "/api/login/metamask";
+                web3Login(login_url, console.log, console.log, console.log, console.log, console.log, function (resp) {
+                    // window.location.replace(resp.redirect_url);
+                });
+            }
+        });
+
+
+
+    } else {
+        alert('web3 missing');
+    }
 
 })
