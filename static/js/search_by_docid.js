b4a796fe75fe35d3b75ea1692b87815847be4327
ANKITSG15 <h20190518@pilani.bits-pilani.ac.in>
2020-05-10 20:01:12 +0530

    user and doc raise request

--- static/js/search_by_docid.js
+++ static/js/search_by_docid.js
@@ -18,19 +18,86 @@ function getDocumentDetails (){
         $("#document_table thead").append(
             `<tr><th>Document Name</th><td>${docs[1]}</td></tr>
             <tr><th>Uploaded Date</th><td>${docs[2]}</td></tr>
-            <tr><th>Uploaded By User Name</th><td>${docs[4]} </td></tr>
+            <tr><th>Uploaded By User Name</th><td>${docs[4]+" "+docs[6]}  </td></tr>
             <tr><th>Uploaded By Email Address</th><td>${docs[5]}</td></tr>
             <tr><th>Contact Details</th><td>${docs[7]}</tr>
-            <tr><td><button class = "btn waves-effect waves-light" id = "doc_${docs[0]}">Raise Request To Access</button></td></tr>`
-            
+            <tr><td><button class = "btn btn-primary sharedoc" doc_id=${docs[0]} doc_name=${docs[1]}>Raise Request To Access</button></td></tr>`
         )
-    
     });
-
-    
 
 }
 
+$(document).on('click', '.sharedoc', function() { 
+    $('#shareDocModel').modal("open"); 
+    var _this = $(this);
+    var doc_id = _this.attr("doc_id");
+    var doc_name = _this.attr("doc_name");
+    $(".doc_name_modal").html("Share doc: " + doc_name)
+    var email = "";
+    contract.methods.getEmailIdByAddrss().call().then(function(_email)
+    {
+            console.log(_email)
+            if(_email != null || _email != "")
+            {
+                email = _email;
+            }
+    });
+
+    $('#shareThisDoc').submit(function(e){
+        e.preventDefault();
+       // var email = $("#share_email_").val();
+       
+        var permission = 0;
+        console.log(email)
+        contract.methods.isValidSharableUser(email).call().then(function(res){
+            if(res){
+                contract.methods.checkAlreadyShared(doc_id, email).call().then(function(res){
+                    if(!res){
+                        contract.methods.shareDocumentwithUser(
+                            doc_id, email, permission
+                        ).send(res).then(function(res){
+                            $('#shareDocModel').modal("close"); 
+                            swal({
+                                title: "Success!",
+                                text: "Shared with " + email,
+                                icon: "success",
+                            });
+                        }).catch(function (error) {
+                            swal({
+                                title: "Error!",
+                                text: "Error while checking user validity " + error,
+                                icon: "error",
+                            });
+                        });
+                    }
+                    else{
+                        swal({
+                            title: "Error!",
+                            text: "This email is already shared with this document",
+                            icon: "error",
+                        });
+                    }
+                });
+            }
+            else{
+                swal({
+                    title: "Error!",
+                    text: "Not a valid email. This email is not a valid or not regsitered!!",
+                    icon: "error",
+                });
+            }
+        }).catch(function (error) {
+            swal({
+                title: "Error!",
+                text: "Error while sharing doc " + error,
+                icon: "error",
+            });
+       });
+
+    })
+
+
+});
 
 $(document).ready(function(){
    
--- static/js/search_by_user.js
+++ static/js/search_by_user.js
@@ -13,9 +13,12 @@ function displayDocumentsList(){
     }    
     
     i = 0;
-        for(var k = 0; k < docs[1].length; k++)
-            documents[i++].timestamp = docs[1][k]
+    for(var k = 0; k < docs[1].length; k++)
+        documents[i++].timestamp = docs[1][k]
     
+    i = 0;
+    for(var k = 0; k < docs[2].length; k++)
+        documents[i++].doc_id = docs[2][k]
 
     $("#document_table thead").append(
         `<tr>
@@ -32,12 +35,11 @@ function displayDocumentsList(){
             <td>Document #${j+1}</td>
             <td>${documents[j].filename}</td>
             <td>${documents[j].timestamp}</td>
-            <td><button class = "btn btn-primary" id = "doc_${j}">Request</button></td>
+            <td><button class = "btn btn-primary sharedoc" doc_id=${documents[j].doc_id}
+             doc_name=${documents[j].filename}>Request</button></td>
             </tr>`
         )
     }
-
-    
     }).catch(function (error) {
         swal({
             title: "Error!",
@@ -60,7 +62,80 @@ function getDocCount(){
    });
 }
 
+$(document).on('click', '.sharedoc', function() { 
+    $('#shareDocModel').modal("open"); 
+    var _this = $(this);
+    var doc_id = _this.attr("doc_id");
+    var doc_name = _this.attr("doc_name");
+    $(".doc_name_modal").html("Share doc: " + doc_name)
+    var email = "";
+    contract.methods.getEmailIdByAddrss().call().then(function(_email)
+    {
+            console.log(_email)
+            if(_email != null || _email != "")
+            {
+                email = _email;
+            }
 
+            
+
+
+    });
+
+    $('#shareThisDoc').submit(function(e){
+        e.preventDefault();
+       // var email = $("#share_email_").val();
+       
+        var permission = 0;
+        console.log(email)
+        contract.methods.isValidSharableUser(email).call().then(function(res){
+            if(res){
+                contract.methods.checkAlreadyShared(doc_id, email).call().then(function(res){
+                    if(!res){
+                        contract.methods.shareDocumentwithUser(
+                            doc_id, email, permission
+                        ).send(res).then(function(res){
+                            $('#shareDocModel').modal("close"); 
+                            swal({
+                                title: "Success!",
+                                text: "Shared with " + email,
+                                icon: "success",
+                            });
+                        }).catch(function (error) {
+                            swal({
+                                title: "Error!",
+                                text: "Error while checking user validity " + error,
+                                icon: "error",
+                            });
+                        });
+                    }
+                    else{
+                        swal({
+                            title: "Error!",
+                            text: "This email is already shared with this document",
+                            icon: "error",
+                        });
+                    }
+                });
+            }
+            else{
+                swal({
+                    title: "Error!",
+                    text: "Not a valid email. This email is not a valid or not regsitered!!",
+                    icon: "error",
+                });
+            }
+        }).catch(function (error) {
+            swal({
+                title: "Error!",
+                text: "Error while sharing doc " + error,
+                icon: "error",
+            });
+       });
+
+    })
+    
+});
 
 $(document).ready(function(){
     //checkAlreadyRegiteredUser()
